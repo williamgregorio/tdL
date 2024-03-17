3d7c39c4a5033655c942a8fb95e0b3c8f5c22899
William Gregorio <mrwilliamgregorio@yahoo.com>
2024-03-17 19:41:08 -0400

    interface addition

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+void interface(){
+  int choice;
+  std::cout << "Please pick a number:\n(1).Test\n" << std::endl;
+  std::cin >> choice;
+  switch(choice) {
+    case 1:
+      std::cout << "test true";
+      break;
+    default:
+      std::cout << "Default touched";
+  }
+}
 int main() {
+  interface();
   return 0;
 }
