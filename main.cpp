908fb1307a9f14c863c189f5eefe92bc61de1daf
William Gregorio <mrwilliamgregorio@yahoo.com>
2024-03-18 02:10:46 -0400

    interface build

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ostream>
 #include <vector>
 
 struct TodoItem {
@@ -7,6 +8,19 @@ struct TodoItem {
   bool completed = false;
 };
 
+std::vector<TodoItem> todoList;
+
+void displayInterface(){
+  std::cout << "tdL" << std::endl;
+  std::cout << "1. Create a new task" << std::endl;
+  std::cout << "2. Display your tasks" << std::endl;
+  std::cout << "3. Delete a task" << std::endl;
+  std::cout << "4. Exit" << std::endl;
+  std::cout << "Enter your choice" << std::endl;
+}
 int main() {
+  while(true){
+    displayInterface();
+  }
   return 0;
 }
